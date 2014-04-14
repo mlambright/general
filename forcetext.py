6b644ff219f0864a45704c83223f0014ca5745b8
Michael Lambright <michaelglambright@gmail.com>
2014-04-14 14:34:34 -0400

    First Commit

--- forcetext.py
+++ forcetext.py
@@ -8,17 +8,19 @@ dictlist = []
 with open(infilename, 'r') as infile:
   drobject = csv.DictReader(infile)
   for row in drobject:
-    print row
     dictlist.append(row)
   headers = drobject.fieldnames
   
 with open(outfilename,'w') as outfile:
-  dwobject = csv.DictWriter(outfile,fieldnames=headers)
+  dwobject = csv.DictWriter(outfile,delimiter=',',lineterminator='\n',fieldnames=headers)
   dwobject.writeheader()
   for row in dictlist:  
     if colname in headers:
-      row[colname] = "'" + str(row[colname])
-      dwobject.writerow(row)
+      if row[colname] == '':
+        dwobject.writerow(row)
+      else:
+        row[colname] = "'" + str(row[colname])
+        dwobject.writerow(row)
     else:
       raise Exception("column name does not exist in file")
 
